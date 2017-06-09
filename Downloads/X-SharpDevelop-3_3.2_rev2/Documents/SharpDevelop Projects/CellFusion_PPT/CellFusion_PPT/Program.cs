8e4a106693a7347729db07a732870f9def006464
Connor Benton <connor.benton@gmail.com>
2017-06-08 18:16:40 -0700

    Beginning to implement renumbering function

---
+++ Downloads/X-SharpDevelop-3_3.2_rev2/Documents/SharpDevelop Projects/CellFusion_PPT/CellFusion_PPT/Program.cs
@@ -0,0 +1,71 @@
+﻿ /*
+ * Created by SharpDevelop.
+ * User: cbenton
+ * Date: 5/26/2017
+ * Time: 3:28 PM
+ */
+
+using System;
+using System.Windows.Forms;
+using System.Linq;
+using System.Threading;
+
+namespace CellFusion_PPT
+{
+			
+	static class Program
+	{
+		public static SplashScreen splashscreen = null;
+		
+		public static MainForm mainForm = null;
+		
+		// Program entry point.
+		[STAThread]
+		public static void Main()
+		{
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+ 
+        //show splash
+        Thread splashThread = new Thread(new ThreadStart(
+            delegate
+            {
+                splashscreen = new SplashScreen();
+                Application.Run(splashscreen);
+            }
+            ));
+        splashThread.SetApartmentState(ApartmentState.STA);
+        splashThread.Start();
+ 
+        //run form - time taking operation
+        mainForm = new MainForm();
+        mainForm.Load += new EventHandler(mainForm_Load);
+        mainForm.Shown += new EventHandler(mainForm_Shown);
+        mainForm.Activate();
+        Application.Run(mainForm);
+		}
+		
+	    static void mainForm_Load(object sender, EventArgs e)
+	    {
+	        //close splash
+	        if (splashscreen == null)
+	        {
+	            return;
+	        }
+	        
+	        splashscreen.Invoke(new Action(splashscreen.Close));
+	        splashscreen.Dispose();
+	        splashscreen = null;
+	        
+	    }
+	    
+	    static void mainForm_Shown(object sender, EventArgs e)
+	    {
+	    	mainForm.WindowState = FormWindowState.Minimized;
+	    	mainForm.Show();
+	    	mainForm.WindowState = FormWindowState.Normal;
+	    }
+	}
+	
+}
+		
---
+++ Downloads/X-SharpDevelop-3_3.2_rev2/Documents/SharpDevelop Projects/CellFusion_PPT/CellFusion_PPT/SplashScreen.Designer.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: cbenton
+ * Date: 6/8/2017
+ * Time: 12:26 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace CellFusion_PPT
+{
+	partial class SplashScreen
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(600, 400);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Loading CellFusion PowerPoint Utility";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// SplashScreen
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.ClientSize = new System.Drawing.Size(600, 400);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "SplashScreen";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Loading CellFusion PowerPoint utility";
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label label1;
+	}
+}
